476388444a532746da403c6f256c58059ff602ac
cr3eperall <davide.michelotti04@gmail.com>
2022-08-02 11:50:30 +0200

    Finished Bar module

--- src/main/java/com/davidemichelotti/peanobar/PeanobarApplication.java
+++ src/main/java/com/davidemichelotti/peanobar/PeanobarApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PeanobarApplication {
+    //TODO add error handling
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeanobarApplication.class, args);
-	}
-        
-        
-
+    public static void main(String[] args) {
+            SpringApplication.run(PeanobarApplication.class, args);
+    }
 }
